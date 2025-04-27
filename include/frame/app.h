f055d965294c85b79f9645ba531ccc9167271f81
Jhonatan <95220392+Naylor-C@users.noreply.github.com>
2025-04-26 21:43:49 -0300

    Update app.h

--- include/frame/app.h
+++ include/frame/app.h
@@ -7,33 +7,45 @@ class App : public Gtk::Window
 
   protected:
     
-  void on_button_clicked();
+  void on_button_start_clicked();
+  voud on_button_options_clicked();
 
-  Gtk:: Button b_search;  
-
+  Gtk:: Button button_start, button_options;  
 };
-
-App:: App()
-{
-  set_title("Basic application");
-  set_default_size(800, 600);
-
-
-}
 
 
 //
-App:: App()
-: b_search("Search")
+App:: App() 
+: button_start("Start"),
+: button_options("Options")
 {
+    set_title("Basic application");
+    set_default_size(800, 600);
 
-    b_search.set_margin(10);
-
+    //layout do button_start//
+    button_start.set_margin(10);
+    button_options.set_margin(10);
+   
     // When the button receives the "clicked" signal, it will call the
     // on_button_clicked() method defined below.
-    b_search.signal_clicked().connect(sigc::mem_fun(*this, &App::on_button_clicked));
+    button_start.signal_clicked().connect(sigc::mem_fun(*this, &App::on_button_start_clicked));
+    button_options.signal_clicked().connect(sigc::mem_fun(*this, &App::on_button_options_clicked));
+
+
+    
   
     // This packs the button into the Window (a container).
-    set_child(b_search);
+    set_child(button_start);
+    set_child(button_options);
+  
+}
+
+void Frame::on_button_start_clicked()
+{
+  
+}
+
+void Frame::on_button_options_clicked()
+{
   
 }
